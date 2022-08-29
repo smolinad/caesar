1dbe174d681ff692678aab7acdd444996669d0d9
smolinad <smolinad@unal.edu.co>
2022-08-28 20:38:42 -0500

    Some changes

--- crypto-flask/algorithms/caesar.py
+++ crypto-flask/algorithms/caesar.py
@@ -4,16 +4,21 @@ import random
 
 ALPHABET = string.ascii_uppercase
 
-def caesar(text:str, key=None):
+def encryptCaesar(text:str, key=None):
   if key is None:
     key = random.randint(0, len(ALPHABET))
-  cypher = ""
-  normalized = ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
-  lowercase = normalized.upper().strip()
-  for c in lowercase:
-      if c != " ":
-        cypher += ALPHABET[(ALPHABET.index(c) + key) % len(ALPHABET)]
-  return cypher
+  elif key.isdigit():
+    key = int(key)
+  if isinstance(key, int):
+    cypher = ""
+    normalized = ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+    lowercase = normalized.upper().strip()
+    for c in lowercase:
+        if c != " ":
+          cypher += ALPHABET[(ALPHABET.index(c) + key) % len(ALPHABET)]
+    return cypher
+  else:
+    print("keyError")
 
 def bruteForceCaesar(text:str):
   for i in range(len(ALPHABET)):
--- crypto-flask/app.py
+++ crypto-flask/app.py
@@ -1,10 +1,8 @@
 from algorithms.caesar import caesar
-from algorithms.vigenere import vigenere
-
 from flask import Flask, request
 from flask.templating import render_template
 
-app = Flask(__name__,
+app= Flask(__name__,
             static_url_path='', 
             static_folder='web/static',
             template_folder='web/templates')
@@ -14,22 +12,17 @@ app = Flask(__name__,
 def home():
     if request.method == 'POST':
         # cypher_mode = request.form.get('cypher_mode')
-        print(request.form.get('caesar'))
-        cypher_mode = "vigenere"
+        cypher_mode = "caesar"
         input_text = request.form.get('input_text')
         input_key = request.form.get('input_key')
 
         match cypher_mode:
             case "caesar":
                 if input_key != '':
-                    output_text = caesar(input_text, int(input_key))
+                    output_text = caesar(input_text, input_key)
                 else:
                     output_text = caesar(input_text)
-            case "vigenere":
-                if input_key != '':
-                    output_text = vigenere(input_text, input_key)
-                else:
-                    output_text = vigenere(input_text)
+
     else:
         return render_template('index.html')
 
