5176bbf27f188ea334f4b92f59f136909d66783b
Your Namembotellom <mbotello@unal.edu.co>
2022-09-12 19:00:33 -0500

    Hill analysis receives text instead of lists

--- crypto-flask/algorithms/hillText.py
+++ crypto-flask/algorithms/hillText.py
@@ -1,90 +1,170 @@
-# hill image
+#Cryptoanalysis de una sola palabra larga?
 
-import cv2 as cv
-import numpy
-import numpy as np
+import math
+import random
+
+import sympy
+#from algorithms.goodies import InputKeyError
 from goodies import InputKeyError
-
 """
-hillEncrypt recibe (s: str, k="") donde s es el nombre de la imagen a encriptar  y k es el nombre de la imagen clave
-no devuelve nada, en lugar de ello guarda las imagenes encrypted y key en la carpeta test-img
+Encrypt y Decrypt reciben (size: int, text: str, key: str)
+ donde size es la dimensión de las matrices
+ y los textos text y key(solo letras mayúsculas)
+ En encrypt se puede no escribir key
+Devuelven lista [encriptedText, key], ambos valores son strings de letras mayúsculas
 
-hillDecrypt recibe (s: str, k: str) donde s y k son los descritos arriba.
-no devuelve nada, en lugar de ello guarda las imagenes decrypted y key en la carpeta test-img
+Cryptoanalysis recibe size: int, encryptedTexts: list, plainTexts: list
+donde size es la dimensión de las matrices
+y encryptedTexts y plainTexts son listas de textos encriptados y claros, respectivamente
+Cada una debe constar de size palabras, de las cuales solo se toma en cuenta las primera size letras
+Devuelve clave
 """
 
-def hillEncrypt(s: str, k=""):
-    mod = 256
-    plain_rectangle = readGray(s)
-
-    n = min(plain_rectangle.shape)
-    if n % 2 != 0:
-        n = n-1
-    plain = plain_rectangle[0:n, 0:n]
-
-    if k == "":
-        m = int(n/2)
-        key_orig = numpy.random.randint(0, mod, (m, m))
+def hillEncrypt( text: str, key = None):
+    if key == None:
+        size = random.randint(2, 4)
+        keyMatrix = randomKeyMatrix(size)
     else:
-        m = int(n/2)
-        key_orig = readGray(k)
-        key_orig = cv.resize(key_orig, (m, m))
+        size = int(math.sqrt(len(key)))
+        checkInput(text, key, size)
+        keyMatrix = getMatrix(key, size)
+        isInvertibleMod(keyMatrix, 26, "key")
 
-    key = involutoryMatrixOf(key_orig)
-    isInvolutory(key, mod)
-    encrypted = np.matmul(plain, key) % mod
+    textMatrix = getTextMatrix(text, size)
+    encryptedMatrix = textMatrix * keyMatrix % 26
 
-    cv.imwrite('test-img/encrypted.png', encrypted)
-    cv.imwrite('test-img/key.png', key_orig)
+    encriptedText = getText(encryptedMatrix)
+    keyText = getText(keyMatrix)
+    return ("".join(encriptedText), keyText)
 
-def hillDecrypt(s: str, k: str):
-    mod = 256
-    encrypted = readGray(s)
-    key_orig = readGray(k)
+def hillDecrypt( text: str, key: str):
+    size = int(math.sqrt(len(key)))
+    keyMatrix = getMatrix(key, size, True)
+    textVector = getTextMatrix(text, size)
 
-    isSquare(encrypted)
-    isSquare(key_orig)
+    isInvertibleMod(keyMatrix, 26, "key")
+    inverseKeyMatrix = keyMatrix.inv_mod(26)
+    decipherMatrix = textVector*inverseKeyMatrix % 26
 
-    key = involutoryMatrixOf(key_orig)
-    isInvolutory(key, mod)
+    inverseKey = getText(inverseKeyMatrix)
+    decryptedText = getText(decipherMatrix)
 
-    if key.shape[0] != encrypted.shape[0]:
-        raise InputKeyError
+    return (decryptedText, inverseKey)
 
-    decrypted = np.matmul(encrypted, key) % mod
+def hillCryptoAnalysis( encryptedLines: str, plainLines: str):
+    encryptedTexts = encryptedLines.split("\n")
+    plainTexts = plainLines.split("\n")
 
-    cv.imwrite('test-img/decrypted.png', decrypted)
-    cv.imwrite('test-img/key.png', key_orig)
+    encryptedTexts = list(filter(lambda i: i != "", encryptedTexts))
+    plainTexts = list(filter(lambda i: i != "", plainTexts))
 
-def isInvolutory(a: numpy.ndarray, mod: int):
-    isSquare(a)
-    y = numpy.identity(a.shape[0])
-    x = numpy.matmul(a % mod, a % mod) % mod
-    if numpy.array_equal(x, y):
-        return
-    raise InputKeyError("Error de código")
+    size = len(encryptedTexts)
+    checkAnalysisInput(encryptedTexts, plainTexts, size)
 
-def involutoryMatrixOf(a11: numpy.ndarray):
-    a22 = -a11
-    y = numpy.identity(a11.shape[0])
-    a12 = y - a11
-    a21 = y + a11
-    a1 = numpy.concatenate((a11, a21))
-    a2 = numpy.concatenate((a12, a22))
-    a = numpy.concatenate((a1, a2), axis=1)
-    return a
+    encrypted_list = []
+    plain_list = []
 
-def isSquare(a: numpy.ndarray):
-    if a.shape[0] != a.shape[1]:
-        raise InputKeyError("Encrypted image and key image must be squared")
+    for i in range(size):
+        encrypted_list.append(getMatrix(encryptedTexts[i][0:size], size))
+        plain_list.append(getMatrix(plainTexts[i][0:size], size))
 
-def readGray(s: str):
-    coloredIm = cv.imread(s)
+    encrypted_matrix = sympy.Matrix(encrypted_list)
+    plain_matrix = sympy.Matrix(plain_list)
+    key_matrix = solveKey(encrypted_matrix, plain_matrix)
+    y = plain_matrix*key_matrix%26
+    return getText(key_matrix)
 
-    if coloredIm is None:
-        raise InputKeyError("There´s no image with the name "+s)
+def randomKeyMatrix(size: int):
+    matrix = sympy.randMatrix(size, min=0, max=25, symmetric=True)
+    while math.gcd(matrix.det(), 26) != 1:
+        matrix = sympy.randMatrix(size, min=0, max=25, symmetric=True)
+    return matrix
 
-    return cv.cvtColor(coloredIm, cv.COLOR_BGR2GRAY)
+#---------------> Matrices
 
-hillEncrypt('test-img/mani.png')
-hillDecrypt('test-img/encrypted.png', 'test-img/key.png')
+def solveKey(encrypted_matrix: sympy.Matrix, plain_matrix: sympy.Matrix):
+    isInvertibleMod(plain_matrix, 26, "plaintexts")
+    inv_plain = plain_matrix.inv_mod(26)
+    y = plain_matrix * inv_plain %26
+    key = inv_plain * encrypted_matrix % 26
+    return key
+
+def isInvertibleMod(matrix: sympy.Matrix, n: int , s=""):
+    if matrix.rows != matrix.cols:
+        raise InputKeyError(s + " associated matrix must be square")
+    det_k = matrix.det()
+    if math.gcd(det_k, n) > 1:
+        raise InputKeyError(
+            "The determinant of the " + s + " associated matrix must be comprime with 26"
+        )
+
+#---------------> Checkers
+
+def checkAnalysisInput(encryptedTexts: list, plainTexts: list, n: int):
+    if n not in range(2, 5):
+        raise InputKeyError("This page works with up to 4 encrypted texts")
+    if len(encryptedTexts) != len(encryptedTexts):
+        raise InputKeyError("Write the same ammount of plain texts and encrypted texts")
+
+def checkInput(text:str, key: str, size:int):
+    if size < 0 or size > 4:
+        raise InputKeyError("Matrix solved up to 4x4")
+    if size*2 > len(key):
+        raise InputKeyError("Key must be " + str(size*2) + " characters long")
+    onlyUppercase_letters(key)
+    # onlyUppercase_letters(text)
+
+def onlyUppercase_letters(s):
+    for i in s:
+        if (ord(i) < 65 or 91 < ord(i)):
+            raise InputKeyError("The key must contain only uppercase letters")
+    return True
+
+#---------------> Converters
+
+def getMatrix(text: list, size: int, square=False):
+    text_l = list(text)
+    matrix_num = []
+    for i in range(0, len(text_l), size):
+        charList = text_l[i:i+size]
+        intList = [ord(j) % 65 for j in charList]
+        matrix_num.append(intList)
+
+        if square and len(intList) != size:
+            raise InputKeyError("Key lenght must be a square")
+
+    return sympy.Matrix(matrix_num)
+
+def getTextMatrix(text: str, size: int):
+    #divide list into strings of size length
+    text_l = [text[i:i+size] for i in range(0, len(text), size)]
+    if len(text_l[-1]) < size:
+        text_l[-1] = text_l[-1] + 'A'*(size-len(text_l[-1]))
+
+    for i in range(len(text_l)):
+        text_l[i] = [ord(i) % 65 for i in text_l[i]]
+
+    return sympy.Matrix(text_l)
+
+def getText(keyMatrix: sympy.Matrix):
+    return "".join([chr(j+65) for j in keyMatrix])
+
+
+
+t = "WORLDISFUNINNOS"
+k = "NINELETTE"
+
+b = hillEncrypt(t)
+bx = hillDecrypt(b[0], b[1])
+
+"Example invertible 3x3 matrix mod 26 fxampjtqo"
+p_t = """
+FXA
+MPJ
+TQOV"""
+e_t = """BHB
+XQS
+FWD"""
+y = [hillEncrypt(i, k) for i in p_t]
+x = hillCryptoAnalysis(e_t, p_t) # Debe ser k= NINELETTE
+#print(x)
