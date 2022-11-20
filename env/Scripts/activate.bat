49f44fe17e4bd894072ac9e0252422a2e852cae2
ancastillov <ancastillov@unal.edu.co>
2022-11-20 13:48:29 -0500

    Gunicorn added

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Documents\crypto_app\crypto-flask\env
+set VIRTUAL_ENV=C:\Users\anapa\OneDrive\Documentos\Universidad\Intro_cripto\crypto_app\env
 
 if not defined PROMPT set PROMPT=$P$G
 
